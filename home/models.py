76c561060bd59cabf223da9b52ff815e96243e4a
Adrian Gasior <colid.bondzio@gmail.com>
2021-02-15 12:05:43 +0100

    added free template for home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    hero_title = models.CharField(
+        max_length=120,
+        blank=True,
+        help_text="Main text displayed in the hero section")
+
+    # Configure admin interface
+    content_panels = Page.content_panels + [
+    	FieldPanel("hero_title"), 
+    ]
+
